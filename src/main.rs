fe2a80da04a27b667c08f7dc2e3f7e6e37a2f260
eugene <eugene@linux.com>
2022-02-01 14:35:33 +0200

    init commit

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use std::path::PathBuf;
+
+use ethers::prelude::Abigen;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+    #[structopt(long = "name")]
+    /// The name of the contract.
+    contract_name: String,
+    #[structopt(long = "abi")]
+    /// The path to the ABI file.
+    abi: String,
+    #[structopt(long = "path")]
+    /// The path to the output file.
+    path: PathBuf,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Cli::from_args();
+    let path = if args.path.is_dir() {
+        args.path.join(args.contract_name.clone() + ".rs")
+    } else {
+        args.path.clone()
+    };
+
+    Abigen::new(&args.contract_name, &args.abi)?
+        .generate()?
+        .write_to_file(&path.as_path())?;
+    Ok(())
+}
+
